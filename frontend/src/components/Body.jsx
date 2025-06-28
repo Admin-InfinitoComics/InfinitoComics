0eced66df6c2e4f1bfa637ed4de879590d5e5d7e
Deepshikha <varshney.deepshikha10@gmail.com>
2025-06-28 22:12:27 +0530

    added loader

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect}  from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../pages/Navbar/Navbar'
 import Footer from '../pages/Footer/Footer'
+import LogoLoader from '../components/loader/logoLoader'; 
 
 const Body = () => {
+
+  const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        setLoading(false);
+      }, 2000); 
+  
+      return () => clearTimeout(timer);
+    }, []);
+    if (loading) {
+      return <LogoLoader />;
+    }
+
   return (
     <div>      
       <Navbar/>

dec6fb45e8326459eadcb11d66c2a840e660db0b
Deepshikha <varshney.deepshikha10@gmail.com>
2025-07-05 18:06:16 +0530

    added npm packages

--- frontend/src/pages/News_Blogs/News.jsx
+++ frontend/src/pages/News_Blogs/News.jsx
@@ -4,7 +4,6 @@ import ArcheryNews from './Archery_News'
 import All_news from './All_news'
 import FoundationSection from './Foundation_Section'
 import ArcherySlider from './ArcherySlider'
-import Spotlight from './Spotlight'
 import Carousel from '../News_Blogs/Carousel'
 const News = () => {
   return (
@@ -12,7 +11,6 @@ const News = () => {
         <Carousel></Carousel>
         <ArcheryNews/>
         <ArcherySlider/>
-        <Spotlight/>
         <All_news/>
         <FoundationSection/>
         <ArcherySlider/>

f041ed0d7aa6d69efb699d88df4b471bce82baab
Snehaaa-Kri <sneha001705@gmail.com>
2025-07-07 17:43:28 +0530

    added mockdata in support us page

--- frontend/src/pages/SupportUs/Credits.jsx
+++ frontend/src/pages/SupportUs/Credits.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import logo from '../../../assets/Images/SupportUs/credit.png';
-
-const companies = {
-    patron: Array(10).fill(logo),
-    titanium: Array(5).fill(logo),
-    platinum: Array(5).fill(logo),
-    gold: Array(6).fill(logo),
-};
+import companies from '../../constants/companies.js';
 
 function Credits() {
     return (

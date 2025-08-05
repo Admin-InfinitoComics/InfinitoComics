821d86f3f9ffd3f5f5ecc510aa948d520cd70de3
Deepshikha <varshney.deepshikha10@gmail.com>
2025-08-05 23:02:12 +0530

    made mobile responsive

--- frontend/src/components/Comics/ComicChap.jsx
+++ frontend/src/components/Comics/ComicChap.jsx
@@ -55,7 +55,7 @@ return (
       {chapters.map((chap) => (
         <div
           key={chap._id}
-          className="group bg-white hover:bg-gray-50 border-1 border-gray-200 transition-all duration-300 hover:shadow-lg"
+          className="group bg-white hover:bg-gray-50 border border-gray-200 transition-all duration-300 hover:shadow-lg"
           onClick={() => {
             setSelectedPdf({ url: chap.chapPdf, title: chap.title });
             navigate(`/comicChap/${comicId}/chapters/pdfView`);
@@ -63,19 +63,19 @@ return (
         >
           <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between gap-4">
             <div className="flex items-start sm:items-center w-full">
-              <div className="w-32 h-17 sm:w-32 sm:h-17 flex-shrink-0 flex items-center justify-center shadow-md">
+              <div className="w-32 h-[68px] sm:w-32 sm:h-[68px] flex-shrink-0 flex items-center justify-center shadow-md">
                 <img
                   src={chap.chapImage}
                   alt="Chapter"
                   className="w-full h-full object-fill"
                 />
               </div>
-              <div className="flex justify-between items-center w-full">
-                <h3 className="text-xl font-medium text-gray-800 truncate max-w-[200px] sm:max-w-xs ml-6">
+              <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center w-full ml-4 sm:ml-6 mt-2 sm:mt-0">
+                <h3 className="text-xl font-medium text-gray-800 truncate max-w-[200px] sm:max-w-xs">
                   {chap.title}
                 </h3>
                 {chap.releaseDate && (
-                  <span className="text-base font-normal text-gray-600 whitespace-nowrap mr-15">
+                  <span className="text-base font-normal text-gray-600 mt-1 sm:mt-0 sm:ml-auto whitespace-nowrap mr-8">
                     {new Date(chap.releaseDate).toLocaleDateString("en-US", {
                       month: "short",
                       day: "numeric",

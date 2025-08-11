3f93fe1bd1352b34ccabe926c58cbf2a3d7b26a9
Lakshya-InfinitoArchery <lakshya2005mudgal@gmail.com>
2025-08-11 23:29:45 +0530

    Research changes for deployment

--- Research/src/utils/constants.js
+++ Research/src/utils/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL="http://localhost:3000";
 export const FRONTEND_BASE_URL="http://localhost:3001";
+export const BACKEND_URL = import.meta.env.VITE_BASE_URL || "http://localhost:3000";

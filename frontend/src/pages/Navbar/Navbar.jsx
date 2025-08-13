e1d8ef96d72890163cc44505684cf6e04a6ebd46
Lakshay Dhall <lakshaydhall9@gmail.com>
2025-08-13 15:33:45 +0530

    added the infinito style link in the shop

--- frontend/src/pages/Navbar/Navbar.jsx
+++ frontend/src/pages/Navbar/Navbar.jsx
@@ -5,9 +5,12 @@ import logo from "../../../assets/Logo.png";
 import { Heart, ShoppingBag } from "lucide-react";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import UserIcon from '../../../assets/Images/UserIcon.png';
-import { RESEARCH_BASE_URL, FOUNDATION_BASE_URL } from "../../utils/constants.js";
-import NavbarShimmer from '../../shimmer/landingPageShimmer/navbarShimmer'
+import UserIcon from "../../../assets/Images/UserIcon.png";
+import {
+  RESEARCH_BASE_URL,
+  FOUNDATION_BASE_URL,
+} from "../../utils/constants.js";
+import NavbarShimmer from "../../shimmer/landingPageShimmer/navbarShimmer";
 
 const Header = () => {
   const [loading, setLoading] = useState(true);
@@ -20,7 +23,9 @@ const Header = () => {
     setTimeout(() => setLoading(false), 2400); // demo
   }, []);
 
-  return loading ? <NavbarShimmer /> : (
+  return loading ? (
+    <NavbarShimmer />
+  ) : (
     <div className="text-white font-sans">
       {/* Top Promo & Links Section */}
       <div className="border-b bg-[#202020] border-gray-600 text-sm px-4 sm:px-8 py-4 flex flex-col md:flex-row justify-around items-center gap-40">
@@ -61,17 +66,16 @@ const Header = () => {
 
         {/* Login Button (only on desktop) */}
 
-
         <div className="hidden cursor-pointer md:block">
           {user ? (
-            <div className="flex items-center gap-2 pointer border border-white px-4 py-2 uppercase text-sm"
-              onClick={() => navigate("/dashboard")}>
-
+            <div
+              className="flex items-center gap-2 pointer border border-white px-4 py-2 uppercase text-sm"
+              onClick={() => navigate("/dashboard")}
+            >
               <img src={UserIcon} alt="User Icon" className="w-5 h-5" />
               <span className="tracking-wide">
                 Hi, {user?.name?.split(" ")[0] || "Guest"}!
               </span>
-
             </div>
           ) : (
             <button
@@ -84,7 +88,7 @@ const Header = () => {
         </div>
 
         {/* Logo Centered */}
-        <Link to='/'>
+        <Link to="/">
           <div className="text-center ">
             <img src={logo} alt="infinto" className=" w-auto" />
           </div>
@@ -152,19 +156,26 @@ const Header = () => {
           <li>
             <Link
               to="/aboutUS"
-
               className="uppercase tracking-wider font-semibold hover:text-white cursor-pointer border-l border-gray-600 px-5"
             >
               About Us
             </Link>
           </li>
           <li>
-            <Link
-              to="/shop"
+            {/* <Link
+              to="/https://www.infinitostyle.com/"
               className="uppercase tracking-wider font-semibold hover:text-white cursor-pointer border-l border-gray-600 px-5 flex items-center gap-2"
             >
               <ShoppingBag /> Shop
-            </Link>
+            </Link> */}
+            <a
+              href="https://www.infinitostyle.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block font-bold hover:text-white flex items-center gap-2"
+            >
+              <ShoppingBag /> SHOP
+            </a>
           </li>
         </ul>
       </div>
@@ -190,16 +201,23 @@ const Header = () => {
           <Link to="/aboutUs" className="block font-bold hover:text-white">
             About Us
           </Link>
-          <Link
+          {/* <Link
             to="/shop"
             className="block font-bold hover:text-white flex items-center gap-2"
           >
             <ShoppingBag /> Shop
-          </Link>
+          </Link> */}
+          <a
+            href="https://www.infinitostyle.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block font-bold hover:text-white flex items-center gap-2"
+          >
+            <ShoppingBag /> SHOP
+          </a>
           <Link to="/blogs" className="block font-bold hover:text-white">
             Blogs & News
           </Link>
-
 
           <a
             href="#"
@@ -223,7 +241,6 @@ const Header = () => {
             Research
           </a>
 
-
           <Link
             to="/support"
             className="font-bold hover:text-white flex items-center gap-2"
@@ -233,7 +250,9 @@ const Header = () => {
           {user ? (
             <div className="flex items-center gap-2 border border-white px-4 py-2 uppercase text-sm">
               <img src={UserIcon} alt="User Icon" className="w-5 h-5" />
-              <span className="tracking-wide">Hi, {user.name.split(" ")[0]}!</span>
+              <span className="tracking-wide">
+                Hi, {user.name.split(" ")[0]}!
+              </span>
             </div>
           ) : (
             <button
